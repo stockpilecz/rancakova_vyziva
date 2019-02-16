0be62ce290acd7cffc66daa73812c49022cbba54
Michal Mrzena <michal.mrzena@gmail.com>
2019-02-16 20:28:36 +0100

    commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 var navbar = document.getElementById('navbar');
 var logoSmall = document.getElementById('logoSmall');
-var main = document.getElementById('main');
+var pageHeader = document.getElementById('pageHeader');
 var checkboxMenu = document.getElementById('checkboxMenu');
 const tabBar = document.getElementById('tab-bar-border');
 const tabs = document.getElementById('tabs');
@@ -10,23 +10,28 @@ function goHome() {
   location.href = "omne.html";
 }
 
-
-// function showMenu() {
-//   document.getElementsByClassName('druhyMenu')[0].style.opacity = 1;
-// }
-
 function displayContent(e, masaz) {
 
-  let boxContent = document.getElementsByClassName('boxContent');
+  const boxContent = document.getElementsByClassName('boxContent');
+  const box = document.getElementsByClassName('box');
+  if(e.currentTarget.className.indexOf('active') > 0) {
+    for (i = 0; i < boxContent.length;i++){
+      boxContent[i].style.display = "none";
+    }
+    for (i = 0; i < box.length; i++) {
+      box[i].className = box[i].className.replace(" active", "");
+    }
+    return
+  }
   for (i = 0; i < boxContent.length;i++){
     boxContent[i].style.display = "none";
   }
-  let box = document.getElementsByClassName('box');
   for (i = 0; i < box.length; i++) {
     box[i].className = box[i].className.replace(" active", "");
   }
-  const masazID = document.getElementById(masaz);
 
+  const masazID = document.getElementById(masaz);
+  
   masazID.style.display = "block";
   tabs.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   e.currentTarget.className += " active";
